19f90c5d1db0d3c0a316c1b3eeb4bb24f0630d53
Julian Reyes Escrigas <julian.reyes.escrigas@gmail.com>
2012-03-02 11:47:31 -0500

    comentarios clase Screen

--- includes/screen.h
+++ includes/screen.h
@@ -19,15 +19,56 @@ namespace snake {
     class Screen
     {
     public:
+        /**
+         * Constructur por defecto de la clase
+        **/
         Screen(){};
+
+        /**
+         * Desructor virtual de la clase
+        **/
         virtual ~Screen(){};
+
+        /**
+         * Metodo que se ejecuta desde el bucle pricipal de la aṕlicacion
+         * tiene la logica a ejecutar de la pantalla
+        **/
         virtual void run();
+
+        /**
+         * Fija el puntero hacia el director de pantallas, esto le permite a la
+         * pantalla tener acceso a todos los recursos que tenga el director de
+         * ventanas
+        **/
         virtual void setDirector(Director *dir){ director = dir;}
+
+        /**
+         * Puntero hacia el director de pantallas
+        **/
         Director *director;
     private:
+        /**
+         * Metodo virtual que devuelve una cadena con el nombre de la pantalla
+        **/
         virtual string getName() = 0;
+
+        /**
+         * Este metodo virtual se ejecuta dentro del metodo run(), y tiene toda la
+         * logica que la pantalla necesita respecto a la entrada
+        **/
         virtual void checkInput() = 0;
+
+        /**
+         * Metodo virtual que realiza la realiza de toda la logica, aqui va
+         * todo lo que sucede en la pantalla, este metodo es llamado desde el
+         * metodo run()
+        **/
         virtual void updateLogic() = 0;
+
+        /**
+         * Este metodo pinta todo en pantalla, este metdoo es llamado desde el
+         * metodo run()
+        **/
         virtual void drawThis() = 0;
     };
 
